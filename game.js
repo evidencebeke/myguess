f91265cd2fa1adcddeee8d1307221481ea8bd396
Beke Ijobra <ijobra20@gmail.com>
2020-09-05 19:05:17 +0100

    Added some changes

--- game.js
+++ game.js
@@ -14,14 +14,24 @@ guessButton.addEventListener('click', function testMatch(){
 
 
     let numberGuessed = parseInt(inputedGuess.value);
-    let numbertoGuess = Math.floor(Math.random()*10+1);
-    if(numberGuessed===numbertoGuess){
-       goodMessage.style.display="block"
-    }
-    else{
-        badMessage.style.display = "block"
-    }
+    let numbertoGuess = Math.floor(Math.random()*100+1);
+    if(typeof(numberGuessed)=== 'number' && numberGuessed >=10 && numberGuessed<=100){
+        if(numberGuessed===numbertoGuess){
+            goodMessage.style.display="block"
+         }
+         else{
+             badMessage.style.display = "block"
+         }
     resultMessage.style.display ='block'
     resultMessage.innerText=`The number to guess was ${numbertoGuess}`
+    }
+
+    else{
+        resultMessage.style.display ='none';
+        goodMessage.style.display = 'none';
+        badMessage.style.display = 'none';
+        alert('See, na number between 10 and 100 I tell you to guess. No dey try find wetin no dey')}
+    
+    
     
 })
